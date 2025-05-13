250bf0343f5f8639d9eec5c87150f89f30f63d11
accidev <accidev@yandex.ru>
2025-05-13 22:05:26 +0300

    fixes

--- AddonUpdater/Program.cs
+++ AddonUpdater/Program.cs
@@ -19,6 +19,20 @@ namespace AddonUpdater
         {      
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += (sender, e) => 
+            {
+                MessageBox.Show($"Произошла ошибка: {e.Exception.Message}\n\nПожалуйста, перезапустите приложение.", 
+                                "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            };
+            
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+            {
+                MessageBox.Show($"Произошла критическая ошибка: {(e.ExceptionObject as Exception)?.Message}\n\nПожалуйста, перезапустите приложение.", 
+                                "Критическая ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            };
+            
             Application.Run(new FormMainMenu());
         }
     }
